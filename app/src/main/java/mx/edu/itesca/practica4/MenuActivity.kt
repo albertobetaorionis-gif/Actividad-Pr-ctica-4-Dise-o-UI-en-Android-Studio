1583adb215104bb6127047917085dcff1519f159
albertobetaorionis-gif <albertobetaorionis@gmail.com>
2026-02-18 19:22:45 -0700

    practica 5

--- app/src/main/java/mx/edu/itesca/practica4/MenuActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica4/MenuActivity.kt
@@ -1,16 +1,36 @@
 package mx.edu.itesca.practica4
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MenuActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
+
+        val btnAntojitos: Button = findViewById(R.id.antojitos)
+        val btnEspecialidades: Button = findViewById(R.id.especialidades)
+
+        btnAntojitos.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Antojitos")
+            startActivity(intent)
+        }
+
+
+        btnEspecialidades.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Especialidades")
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
