461fdc3b37b3b7138855a9c3e5caac13082daf4a
albertobetaorionis-gif <albertobetaorionis@gmail.com>
2026-02-19 09:23:24 -0700

    Actualización Práctica 5

--- app/src/main/java/mx/edu/itesca/practica4/MenuActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica4/MenuActivity.kt
@@ -17,6 +17,10 @@ class MenuActivity : AppCompatActivity() {
 
         val btnAntojitos: Button = findViewById(R.id.antojitos)
         val btnEspecialidades: Button = findViewById(R.id.especialidades)
+        val btnCombinations: Button = findViewById(R.id.combinations)
+        val btnTortas: Button = findViewById(R.id.tortas)
+        val btnSopas: Button = findViewById(R.id.sopas)
+        val btnDrinks: Button = findViewById(R.id.drinks)
 
         btnAntojitos.setOnClickListener {
             val intent = Intent(this, ProductosActivity::class.java)
@@ -24,13 +28,35 @@ class MenuActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
         btnEspecialidades.setOnClickListener {
             val intent = Intent(this, ProductosActivity::class.java)
             intent.putExtra("menuType", "Especialidades")
             startActivity(intent)
         }
 
+        btnCombinations.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Combinations")
+            startActivity(intent)
+        }
+
+        btnTortas.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Tortas")
+            startActivity(intent)
+        }
+
+        btnSopas.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Sopas")
+            startActivity(intent)
+        }
+
+        btnDrinks.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Drinks")
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/mx/edu/itesca/practica4/ProductosActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica4/ProductosActivity.kt
@@ -1,29 +1,48 @@
 package mx.edu.itesca.practica4
 
 import android.content.Context
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
 
 class ProductosActivity : AppCompatActivity() {
+
     var menu: ArrayList<Product> = ArrayList<Product>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_productos)
-        var menuOption: String? = intent.getStringExtra("menuType")
+
+        // 🔹 Obtener el tipo de menú enviado desde MenuActivity
+        val menuOption: String? = intent.getStringExtra("menuType")
+
+        // 🔹 CAMBIAR TITULO
+        supportActionBar?.title = menuOption
+
+        // 🔹 CAMBIAR IMAGEN SUPERIOR
+        val imagen : ImageView = findViewById<ImageView>(R.id.imagenView)
+        when(menuOption){
+            "Antojitos" -> imagen.setImageResource(R.drawable.antojitos)
+            "Especialidades" -> imagen.setImageResource(R.drawable.especialidades)
+            "Combinations" -> imagen.setImageResource(R.drawable.combinations)
+            "Tortas" -> imagen.setImageResource(R.drawable.tortas)
+            "Sopas" -> imagen.setImageResource(R.drawable.sopas)
+            "Drinks" -> imagen.setImageResource(R.drawable.drinks)
+        }
+
+        // 🔹 Cargar productos
         agregarProductos(menuOption)
-        var listView: ListView = findViewById(R.id.listView) as ListView
-        var adaptador: AdaptadorProductos = AdaptadorProductos (this, menu)
+
+        // 🔹 Mostrar lista
+        val listView: ListView = findViewById(R.id.listView)
+        val adaptador: AdaptadorProductos = AdaptadorProductos(this, menu)
         listView.adapter = adaptador
     }
 
@@ -53,42 +72,132 @@ class ProductosActivity : AppCompatActivity() {
                 menu.add(Product("Mojarra Frita", R.drawable.mojarra,
                     "Tilapia frita servida con lechuga, cebolla, jitomate, aguacate y tortillas. Fried tilapia served with lettuce, onions, tomatoes, avocado and tortillas.",
                     17.99))
+                menu.add(Product("Ceviche", R.drawable.ceviche,
+                    "Tilapia, camarones, aguacate y pico de gallo servido con chips. Tilapia, shrimp, avocado and pico de gallo, served with chips.",
+                    6.99))
+                menu.add(Product("Botana De Camarones Al Mojo De Ajo", R.drawable.botanacamarones,
+                    "",
+                    19.99))
+            }
+            "Combinations"->{
+                menu.add(Product("Traditional Taco Combo", R.drawable.taco,
+                    "Tortilla de maíz y tu carne favorita. Servido con arroz y frijoles. Corn taco, your choice of meat, served with rice and beans.",
+                    6.99))
+                menu.add(Product("Mexican Burritos", R.drawable.burritos,
+                    "Tu carne favorita en una tortilla grande de harina rellena con cebolla frita, pico de gallo, sour cream, frijoles y acompañado de arroz. Your favorite meat in a large flour tortilla filled with fried onions, pico de gallo, sour cream, beans and served with rice.",
+                    13.69))
+                menu.add(Product("Los Portales Burritos", R.drawable.burritosportales,
+                    "Tortilla de harina grande rellena de nopal asado, bistec, pastor, cebollas asadas, arroz y frijoles, servida con una guarnición de ensalada de lechuga. Large flour tortilla filled with grilled cactus, steak, pastor (seasoned pork), grilled onions, rice and beans, served with a side of lettuce salad.",
+                    6.99))
+                menu.add(Product("Cinco", R.drawable.cinco,
+                    "Un taco de venta fuerte con su elección de carne, lechuga y queso. Un burrito con su elección de carne, pico de gallo, crema agria y frijoles. Servido con arroz y frijoles. One hard sell taco with your choice of meat, lettuce and cheese. One burrito with your choice of meat, pico de gallo, sour cream, and beans. Served with rice and beans.",
+                    6.99))
+                menu.add(Product("Dos Amigos", R.drawable.dosamigos,
+                    "Dos tacos de tortilla dura con la carne de su elección cubiertos con lechuga y queso rallado, servidos con un arroz y frijoles. Two hard shell tacos with your choice of meat topped with lettuce and shredded cheese, served with a side of rice and beans.",
+                    10.99))
+            }
+            "Tortas"->{
+                menu.add(Product("Torta Regular", R.drawable.torta,
+                    "Todas las tortas vienen con lechuga, cebolla, tomate, aguacate, crema agria, chile jalapeño y la carne de su elección. All tortas come with lettuce, onions, tomatoes, avocado, sour cream, jalapeno pepper and your choice of meat.",
+                    13.39))
+                menu.add(Product("Torta - Milanesa de Res o Pollo", R.drawable.tortamilanesa,
+                    "",
+                    13.69))
+                menu.add(Product("Torta Cubana", R.drawable.tortacubana,
+                    "Chorizo, asada, jamon, pastor y queso. Mexican sausage, steak, marinated pork, ham, and cheese.",
+                    14.99))
+                menu.add(Product("Torta Mixta", R.drawable.tortamixta,
+                    "Chorizo, asada y pastor. Mexican sausage, steak and marinated pork.",
+                    13.99))
+                menu.add(Product("Small Mexican Torta", R.drawable.smallmexican,
+                    "Sándwich de pan bolillo con tu de carne, lechuga rallada, aguacate, tomate, cebolla y crema agria. Bolillo bread sandwich with your of meat, shredded lettuce, avocado, tomato, onion and sour cream.",
+                    11.99))
+            }
+            "Sopas"->{
+                menu.add(Product("Pozole", R.drawable.pozole,
+                    "MIÉRCOLES - DOMINGO: Costillas de cerdo y estofado con maíz y orégano. ¡NO OLVIDEN NUESTRA BARRA DE SALSAS PARA AÑADIRLE MÁS SABOR!WEDNSDAY - SUNDAY Pork ribs and bonston butt stew with homminy and oregano. DO NOT FORGET OUR SALSA BAR TO MAKE IT MORE FLAVORFULL.",
+                    7.99))
+                menu.add(Product("Menudo", R.drawable.menudo,
+                    "MENUDO Caldo ancestral, callos sedosos, se unen en un plato que sabe a mañanas de domingo, a cocinas familiares y a historias transmitidas de generación en generación. MENUDO Ancestral broth, silky tripe, come together in a bowl that tastes like Sunday mornings, family kitchens, and stories passed down,",
+                    7.99))
+                menu.add(Product("Caldo de Res", R.drawable.caldores,
+                    "Costillas de res y guiso con maíz, papa, chayote, repollo, cilantro, cebolla, calabacín, zanahorias, limón y tortillas. Beef ribs and chuck stew with corn, potato, chayote (squash family), cabage, cilantro, onion, zuccini, carrots, lime, and tortillas. ",
+                    5.99))
+                menu.add(Product("Caldo de Camarón", R.drawable.caldocamaron,
+                    "Sopa de Camarón. Shrimp soup.",
+                    13.99))
+                menu.add(Product("Sopa de Mariscos", R.drawable.sopamariscos,
+                    "Sopa de Mariscos. Seafood soup.",
+                    21.99))
+            }
+            "Drinks"->{
+                menu.add(Product("Michelada", R.drawable.michelada,
+                    "",
+                    3.75))
+                menu.add(Product("Jarritos", R.drawable.jarritos,
+                    "",
+                    5.75))
+                menu.add(Product("Caguama", R.drawable.caguama,
+                    "",
+                    5.75))
+                menu.add(Product("Caguamas Micheladas", R.drawable.caguamamichelada,
+                    "",
+                    2.75))
+                menu.add(Product("Charolazo", R.drawable.charolazo,
+                    "",
+                    9.75))
+                menu.add(Product("Cubeta Siz", R.drawable.cubetasix,
+                    "",
+                    9.75))
+                menu.add(Product("Cubetazo", R.drawable.cubetazo,
+                    "",
+                    9.75))
+                menu.add(Product("Mexican Beer", R.drawable.mexicanbeer,
+                    "",
+                    2.75))
+                menu.add(Product("Sodas", R.drawable.sodas,
+                    "",
+                    2.75))
             }
         }
     }
 
-
     private class AdaptadorProductos: BaseAdapter {
         var producto=ArrayList<Product>()
         var contexto: Context? = null
+
         constructor(contexto: Context, producto: ArrayList<Product>){
             this.producto=producto
             this.contexto=contexto
         }
 
-
         override fun getCount(): Int {
             return producto.size
         }
+
         override fun getItem(position: Int): Any {
             return producto[position]
         }
+
         override fun getItemId(position: Int): Long {
             return position.toLong()
         }
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
-            var prod=producto [position]
-            var inflador= LayoutInflater.from(contexto)
-            var vista=inflador.inflate(R.layout.producto_view, null)
-            var imagen=vista.findViewById(R.id.producto_img) as ImageView
-            var nombre=vista.findViewById(R.id.producto_nombre) as TextView
-            var desc=vista.findViewById(R.id.producto_desc) as TextView
-            var precio=vista.findViewById(R.id.producto_precio) as TextView
+            val prod = producto[position]
+            val inflador = LayoutInflater.from(contexto)
+            val vista = inflador.inflate(R.layout.producto_view, null)
+
+            val imagen = vista.findViewById<ImageView>(R.id.producto_img)
+            val nombre = vista.findViewById<TextView>(R.id.producto_nombre)
+            val desc = vista.findViewById<TextView>(R.id.producto_desc)
+            val precio = vista.findViewById<TextView>(R.id.producto_precio)
+
             imagen.setImageResource(prod.image)
-            nombre.setText(prod.name)
-            desc.setText(prod.description)
-            precio.setText("$${prod.price}")
+            nombre.text = prod.name
+            desc.text = prod.description
+            precio.text = "$${prod.price}"
+
             return vista
         }
     }
